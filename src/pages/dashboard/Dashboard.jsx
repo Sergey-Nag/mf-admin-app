76949b7c5614eae23fb44ef33825de5d8a52c003
Serhii Nahornyi <serhii.nahornyi@zerto.com>
2023-08-27 19:16:29 +0300

    some changes

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import styles from './Dashboard.css';
 
 export function Dashboard() {
-    const items = ['0', '1', '2', '3', '4'];
     return (
         <>
-            <h2>Dashboard</h2>
-            <ol>
-                {items.map((id) => (
-                    <li key={id}>
-                        <Link to={id}>
-                            Go to a page №
-                            {id}
-                        </Link>
-                    </li>
-                ))}
-            </ol>
-            <hr />
-            <Outlet />
-            <Link to="/">Got to main</Link>
+            
+            Dashboard page
+            <Link to="/">Main</Link>
         </>
     );
 }

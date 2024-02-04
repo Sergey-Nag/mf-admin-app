9697c290293cb040c0a6d483dcf2ac53665cbda7
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2024-02-04 23:49:54 +0200

    fix status color & load dashboard numbers

--- src/pages/orders/cell-renderers/StatusCellRenderer.jsx
+++ src/pages/orders/cell-renderers/StatusCellRenderer.jsx
@@ -3,13 +3,13 @@ import { tss } from 'tss-react/mui';
 import { ORDER_STATUS } from '../../../constants/api';
 
 const statusColor = {
-    [ORDER_STATUS.NEW]: 'yellow',
-    [ORDER_STATUS.VERIFIED]: 'green',
-    [ORDER_STATUS.PACKED]: 'lightblue',
-    [ORDER_STATUS.SHIPPED]: 'purple',
+    [ORDER_STATUS.NEW]: '#ffe200',
+    [ORDER_STATUS.VERIFIED]: '#04a904',
+    [ORDER_STATUS.PACKED]: '#70dcff',
+    [ORDER_STATUS.SHIPPED]: '#aa07aa',
     [ORDER_STATUS.DELIVERED]: '#0ADFD2',
-    [ORDER_STATUS.CANCELED]: 'grey',
-    [ORDER_STATUS.RETURNED]: 'red',
+    [ORDER_STATUS.CANCELED]: '#808080',
+    [ORDER_STATUS.RETURNED]: '#ff0000',
 };
 
 const useStyles = tss.create(({ value, theme }) => ({
@@ -20,9 +20,7 @@ const useStyles = tss.create(({ value, theme }) => ({
         padding: theme.spacing(1, 2),
     },
     statusText: {
-        color: 'grey',
-        opacity: '0.5',
-        mixBlendMode: 'multiply',
+        color: theme.palette.getContrastText(statusColor[value]),
     },
 }));
 
